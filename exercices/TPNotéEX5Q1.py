74312ee084a6bd7f2663473bfe77b93d8fb858ac
carbo <carbo@Dell>
2020-02-15 22:45:39 +0100

    update

--- exercices/TPNotéEX5Q1.py
+++ exercices/TPNotéEX5Q1.py
@@ -8,7 +8,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from _collections import defaultdict
 
-def imgproc15():
+def imgproc15():   
     src = cv2.VideoCapture('sunrise.mp4')
     ret, img_prec = src.read()
     img_prec = cv2.cvtColor(img_prec,cv2.COLOR_BGR2GRAY)
@@ -19,10 +19,9 @@ def imgproc15():
     seuil_diff = 100000
     
     liste_de_plan = []
-    liste_img_de_plan = []
+    liste_img = []
     nb_img = 0
     while ret == True:
-        
         ret , img_suiv = src.read()
         img_suiv = cv2.cvtColor(img_suiv,cv2.COLOR_BGR2GRAY)
         img_suiv = cv2.resize(img_suiv,(480,320))
@@ -31,10 +30,17 @@ def imgproc15():
         if non_zero_val > seuil_diff:#plan ici 
             img_prec = img_suiv
             i+=1
-            print("Plan N°",i,"-->",nb_img,"images")
+            liste_de_plan.append([i,img_prec])
+            #print("Plan N°",i,"-->",nb_img,"images")
             nb_img=0
-        #liste_img_de_plan.append(img_suiv)
+        liste_img.append([i,img_suiv])
         nb_img+=1
-        
-                 
+    
+        if i == 30:
+            break    
+    for j in range(0,len(liste_de_plan)):
+        for k in range(0,len(liste_img)):
+            if(j == liste_img[k][0]):
+                print(j,k)
+                     
 imgproc15()
