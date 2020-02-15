13d209843b20e1cabd51b8f3a046c0ec866e349f
carbo <carbo@Dell>
2020-02-15 06:44:30 +0100

    Amélioration de la fonction d'affichage imgprc12_v2

--- exercices/TPNotéEX3Q2.py
+++ exercices/TPNotéEX3Q2.py
@@ -1,10 +1,8 @@
 '''
-Created on 15 févr. 2020
+Created on 15 fevr. 2020
 
 @author: carbo
 '''
-
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -12,10 +10,24 @@ from matplotlib import pyplot as plt
 #Substraction
 def imgproc12(img1,img2):
     return cv2.absdiff(img1,img2) 
-# retourne la différence entre deux images passé en arguement
+# retourne la difference entre deux images passe en arguement
+
+def imgproc12_v2(img1,img2):
+    img_resu = cv2.absdiff(img1,img2)
+    titles = ['Roi de pique','Roi de carreau', 'DiffÃ©rence']# Titre imgss
+    images = [img1, img2, img_resu]#Liste img
+    for i in range(3):#affichage des image dans l'ordre
+        plt.subplot(1,3,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
+    
 
 while (True):
-    imgproc12()
+    img1 = cv2.imread('diff1.png',0)
+    img2 = cv2.imread('diff2.png',0)
+    #cv2.imshow('Differences',imgproc12(img1,img2))
+    imgproc12_v2(img1,img2)
     if cv2.waitKey(1000) | 0xFF == ord('q'):
         break
     break
