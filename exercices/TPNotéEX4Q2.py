d56eaac5190e7cf800889104dc03613b36069488
carbo <carbo@Dell>
2020-02-15 20:53:45 +0100

    Q2 TERMINATED GO TO NEXT EXERCICE --> 5

--- exercices/TPNotéEX4Q2.py
+++ exercices/TPNotéEX4Q2.py
@@ -13,7 +13,7 @@ def imgproc14():
     img_prec = cv2.cvtColor(img_prec,cv2.COLOR_BGR2GRAY)
     img_prec = cv2.resize(img_prec,(480,320))
     i = 0 
-    seuil_diff = 151000
+    seuil_diff = 100000
     while ret == True:
         ret , img_suiv = src.read()
         img_suiv = cv2.cvtColor(img_suiv,cv2.COLOR_BGR2GRAY)
@@ -21,15 +21,16 @@ def imgproc14():
         diff = cv2.absdiff(img_prec,img_suiv)
         non_zero_val = np.count_nonzero(diff)
         if non_zero_val > seuil_diff: 
-            cv2.imshow("img_prec", img_prec)
-            cv2.imshow("img_suiv", img_suiv)
             img_prec = img_suiv
             i+=1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print(i)
+        cv2.imshow("img_prec", img_prec)
+        cv2.imshow("img_suiv", img_suiv)
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             src.release()
             cv2.destroyAllWindows()
             break
-        print("fin iteration")
-         
+                 
 imgproc14()
         
