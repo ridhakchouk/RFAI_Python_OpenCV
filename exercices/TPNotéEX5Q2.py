1ed5d6d9723ed9e8592951fbaeda7df517eb2caf
carbo <carbo@Dell>
2020-02-16 04:17:12 +0100

    amélioration

--- exercices/TPNotéEX5Q2.py
+++ exercices/TPNotéEX5Q2.py
@@ -3,3 +3,17 @@ Created on 16 févr. 2020
 
 @author: carbo
 '''
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from _collections import defaultdict
+
+def imgproc16():   
+    img = cv2.imread('home.jpg')
+    gray= cv2.cvtColor(img,cv.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp = sift.detect(gray,None)
+    img=cv2.drawKeypoints(gray,kp,img)
+    cv2.imwrite('sift_keypoints.jpg',img)
+
+imgproc16()
